9e66288525e261fa592b23319ffebb184ae94ec6
tobias odion <tobiasodion@gmail.com>
2023-12-28 00:40:32 +0100

    Complete MVP

---
+++ components/Question.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { askQuestion } from "@/utils/api"
+import { useState } from "react"
+
+const Question = () => {
+    const [value, setValue] = useState()
+    const [loading, setLoading] = useState(false)
+    const [response, setResponse] = useState()
+
+    const onchange = async (e) => {
+        e.preventDefault()
+        setValue(e.target.value)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        setResponse('')
+        const answer = await askQuestion(value)
+        setResponse(answer)
+        setLoading(false)
+
+    }
+
+    return <div>
+        <form onSubmit={handleSubmit}>
+            <input
+                disabled={loading}
+                className="border border-black/20 px-4 py-2 text-lg rounded-lg"
+                onChange={onchange}
+                value={value}
+                type="text"
+                placeholder="Ask a question" />
+            <button disabled={loading} className="bg-blue-400 px-4 py-2 rounded-lg text-lg">
+                Ask
+            </button>
+        </form>
+        {loading && <div>...loading</div>}
+        {response && <div>{response}</div>}
+    </div>
+}
+
+export default Question
